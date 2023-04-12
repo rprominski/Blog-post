13b3a01e7b86546cd047eabd068b1359a254bec4
Rafał Prominski <prominski.rafal@gmail.com>
2023-04-12 20:33:39 +0200

    add getters and setter to User

--- src/my/better/solution/User.java
+++ src/my/better/solution/User.java
@@ -2,15 +2,32 @@ package my.better.solution;
 
 import java.util.UUID;
 
+import static my.better.solution.Require.require;
+
 class User {
     private UUID id;
     private String username;
     private UserType type;
 
-    // ...
 
-    UserType getType() {
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserType getType() {
         return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
     }
 
     boolean canChangeUsername() {
@@ -20,7 +37,6 @@ class User {
     void changeUsername(String updatedUsername) {
         // fails if not met! (domain assertion)
         require(canChangeUsername(), "Illegal username change");
-
         this.username = updatedUsername;
     }
 }
